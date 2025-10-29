df1f74768948b821436a75dcd50081cb7544bc9d
Student User <student@workstation.lab.example.com>
2025-10-29 03:25:39 -0400

    first

---
+++ kafka-producers/plain/producer/src/main/java/com/redhat/telemetry/producer/ProducerApp.java
@@ -0,0 +1,74 @@
+package com.redhat.telemetry.producer;
+
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SslConfigs;
+
+public class ProducerApp {
+    public static Properties configureProperties() {
+        Properties props = new Properties();
+
+        // TODO: configure the bootstrap server
+	props.put(
+	ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+		            "my-cluster-kafka-bootstrap-tkcyzz-kafka-cluster.apps.ap46r.prod.ole.redhat.com"
+        );
+        // TODO: configure the key and value serializers
+        props.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringSerializer"
+        );
+        props.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.IntegerSerializer"
+        );
+    
+        // TODO: configure the SSL connection
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        props.put(
+                SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                "ABSOLUTE_PATH_TO_YOUR_WORKSPACE_FOLDER/truststore.jks"
+        );
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "password");
+    	//
+    
+        // TODO: configure the SSL connection
+
+        return props;
+    }
+
+    public static void main(String[] args) {
+        // TODO: Implement the Kafka producer
+	//
+        Random random = new Random();
+        Producer<Void,Integer> producer = new KafkaProducer<>(
+                 configureProperties()
+        );
+    
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<Void, Integer> record = new ProducerRecord<>(
+                    "total-connected-devices",
+                    random.nextInt(100)
+            );
+    
+            producer.send(record);
+            printRecord(record);
+        }
+    
+        producer.close();	
+    }
+
+    private static void printRecord(ProducerRecord record) {
+        System.out.println("Sent record:");
+        System.out.println("\tTopic = " + record.topic());
+        System.out.println("\tPartition = " + record.partition());
+        System.out.println("\tKey = " + record.key());
+        System.out.println("\tValue = " + record.value());
+    }
+}
---
+++ streams-interaction/src/main/java/com/redhat/wallet/PaymentsStream.java
@@ -0,0 +1,45 @@
+package com.redhat.wallet;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+
+
+@ApplicationScoped
+public class PaymentsStream {
+
+    // Deserializer for message keys.
+    private final Serde<String> keySerde = Serdes.String();
+
+    // Serializer for message values
+    private final Serde<Integer> valueSerde = Serdes.Integer();
+
+    @Produces
+    public Topology buildTopology() {
+        // TODO: Create the stream from the "payments" topic
+	StreamsBuilder builder = new StreamsBuilder();
+	KStream<String, Integer> stream = builder.stream(
+			        "payments", Consumed.with(keySerde, valueSerde)
+				    );
+
+        // TODO: use foreach to print each message
+        stream.foreach(
+            (key, value) -> System.out.println("Received payment: " + value)
+        );	
+
+        // TODO: process the stream and send the result to the "large-payments" topic
+	stream
+		            .filter((key, amount) -> amount > 1000)
+			            .to("large-payments", Produced.with(keySerde, valueSerde));
+
+        // TODO: return the topology
+        return builder.build();
+    }
+}
